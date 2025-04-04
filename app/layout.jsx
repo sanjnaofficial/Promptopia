58b1d513a2f50bd41c4dd2b2e2a03dddc54516c2
Sanjna <sanjna.contact@gmail.com>
2025-04-04 16:24:52 +0530

    Project features, routes: Feed, Form, Profile, Nav

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,6 @@
 import '@styles/global.css'
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
 
 export const metadata={
     title: "Promptopia",
@@ -8,12 +10,15 @@ const RootLayout = ({children}) => {
   return (
     <html lang='en'>
         <body>
-            <div className='main'>
-                <div className='gradient'></div>
-            </div>
-            <main className='app'>
-                {children}
-            </main>
+            <Provider>
+                <div className='main'>
+                    <div className='gradient'></div>
+                </div>
+                <main className='app'>
+                    <Nav />
+                    {children}
+                </main>
+            </Provider>
         </body>
     </html>
   )
