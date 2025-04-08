fd9563c32297626c7ccbb072965706c98254f372
Sanjna <sanjna.contact@gmail.com>
2025-04-09 00:19:04 +0530

    username regex operation changed

--- models/user.js
+++ models/user.js
@@ -10,7 +10,10 @@ const UserSchema = new Schema({
         type: String,
         unique: [true, 'Username already exists!'],
         required: [true, 'Username is required!'],
-        match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
+        match: [
+            /^(?=.{3,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
+            "Username invalid, it should be 3-20 characters, alphanumeric, and cannot start or end with _ or ."
+        ]
     },
     image:{
         type: String
